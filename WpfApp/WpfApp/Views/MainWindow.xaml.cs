c5a370d7c53985e08217f558d2ee783c60fe620f
Walsonus <walkiewiczbartosz@gmail.com>
2025-04-29 19:56:16 +0200

    Add MainWindow layout

---
+++ WpfApp/WpfApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp.ViewModels;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+        // Reservation button
+        private void resBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Przejście do innego okna");
+        }
+
+        // Exit button
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Czy na pewno chcesz wyjść?", "Pytanie", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        
+    }
+}
