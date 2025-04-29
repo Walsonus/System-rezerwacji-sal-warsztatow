4bf1b2505a80e007ee1b031ecd400025a4f0adbd
Walsonus <walkiewiczbartosz@gmail.com>
2025-04-29 12:27:45 +0200

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Plat_dot_NET_32INF_SSI_NP_PROJEKT_Bartosz_Walkiewicz_Krzysztof_Sokalski
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
